3e2b9d547832bc4ccb81a7b6456f081a95c3f87c
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-09-13 15:02:18 -0300

    Delete World 1 /(0.3) Sum.py

--- World 1 /(0.3) Sum.py
+++
@@ -1,4 +0,0 @@
-numero1=int(input('Digite um número:'))
-numero2=int(input('Digite outro número:'))
-soma=(numero1+numero2)
-print ('O resultado entre {} e {} é igual a {}'. format (numero1, numero2, soma))
