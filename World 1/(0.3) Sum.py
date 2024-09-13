845495a15d8802f596dac62cdf8431d4458c21ad
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-09-13 15:02:37 -0300

    Create (0.3) Sum.py

---
+++ World 1/(0.3) Sum.py
@@ -0,0 +1,4 @@
+numero1=int(input('Digite um número:'))
+numero2=int(input('Digite outro número:'))
+soma=(numero1+numero2)
+print ('O resultado entre {} e {} é igual a {}'. format (numero1, numero2, soma))
